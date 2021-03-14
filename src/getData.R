8b79b9c196be8fb16a61f82be3bde320f4d8beb8
jvfe <jvfecav@gmail.com>
2021-03-14 14:06:10 -0300

    feat: Add initial script for acquiring data

---
+++ src/getData.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(lubridate)
+library(rtweet)
+
+secrets <- Sys.getenv(c("TWITTER_KEY", "TWITTER_SECRET", "ACCESS_TOKEN", "ACCESS_SECRET"))
+current_date <- date(Sys.Date())
+current_tweet_data <- read_twitter_csv("../data/rstats_tweets.csv")
+
+
+token <- create_token(
+  app = "rtweet-exploration",
+  consumer_key = secrets["TWITTER_KEY"],
+  consumer_secret = secrets["TWITTER_SECRET"],
+  access_token = secrets["ACCESS_TOKEN"],
+  access_secret = secrets["ACCESS_SECRET"]
+)
+
+latest_3000 <- search_tweets(
+  "#rstats",
+  n = 3000, include_rts = FALSE,
+) %>%
+  flatten()
+
+all_data <- current_tweet_data %>%
+  mutate(
+    created_at = as_datetime(created_at),
+    quoted_created_at = as_datetime(quoted_created_at),
+    account_created_at = as_datetime(account_created_at)
+  ) %>%
+  bind_rows(latest_3000) %>%
+  distinct(created_at, user_id, text, .keep_all = TRUE)
+
+all_data %>%
+  write_as_csv(file_name = "../data/rstats_tweets")

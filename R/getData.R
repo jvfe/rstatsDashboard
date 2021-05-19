f04488a4089d8b374c40322d515c8c4750d26934
jvfe <jvfecav@gmail.com>
2021-05-19 08:27:45 -0300

    feat: Add latest posts from rbloggers in JSON
    Also some data cleaning for better performance / smaller files

--- R/getData.R
+++ R/getData.R
@@ -1,10 +1,23 @@
 library(dplyr)
 library(lubridate)
+library(jsonlite)
 library(rtweet)
+library(janitor)
 library(dotenv)
+library(logger)
 
-secrets <- Sys.getenv(c("TWITTER_KEY", "TWITTER_SECRET", "ACCESS_TOKEN", "ACCESS_SECRET"))
-current_date <- date(Sys.Date())
+log_threshold(INFO)
+
+#### Initial setup ####
+
+secrets <-
+  Sys.getenv(c(
+    "TWITTER_KEY",
+    "TWITTER_SECRET",
+    "ACCESS_TOKEN",
+    "ACCESS_SECRET"
+  ))
+
 current_tweet_data <- read_twitter_csv("data/rstats_tweets.csv")
 
 token <- create_token(
@@ -15,11 +28,13 @@ token <- create_token(
   access_secret = secrets["ACCESS_SECRET"]
 )
 
-latest_3000 <- search_tweets(
-  "#rstats",
-  n = 3000, include_rts = FALSE,
-) %>%
-  flatten()
+#### Updating .csv tweet data ####
+
+log_info("Updating dataset with latest 3000 tweets")
+
+latest_3000 <- search_tweets("#rstats",
+                             n = 3000, include_rts = FALSE) %>%
+  rtweet::flatten()
 
 all_data <- current_tweet_data %>%
   mutate(
@@ -28,7 +43,21 @@ all_data <- current_tweet_data %>%
     account_created_at = as_datetime(account_created_at)
   ) %>%
   bind_rows(latest_3000) %>%
-  distinct(created_at, user_id, text, .keep_all = TRUE)
+  distinct(created_at, user_id, text, .keep_all = TRUE) %>% 
+  remove_empty(which="cols")
 
 all_data %>%
   write_as_csv(file_name = "data/rstats_tweets")
+
+#### Updating Rbloggers JSON data ####
+
+log_info("Updating Rbloggers dataset with more recent posts")
+
+rbloggers_latest <-
+  get_timeline("Rbloggers", n = 1000, retryonratelimit = TRUE) %>% 
+  filter(created_at >= today()-30) %>% 
+  remove_empty(which="cols")
+
+rbloggers_json <- toJSON(rbloggers_latest, pretty = TRUE)
+
+write(rbloggers_json, "data/rbloggers.json")
